95cc101da17be71e99622ba699abb05edf34b16d
anurithaj <anurithajuturu@gmail.com>
2024-08-25 16:30:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting us! We will get back to you soon.');
+    // Here you could add code to actually send the form data to your backend
+});
